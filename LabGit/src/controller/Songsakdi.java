80a804aedacc788087ad6279e7b0da67205b438e
Jake <User@Room111-25>
2017-11-10 12:33:38 +0700

    add JUnit test for class

--- LabGit/src/controller/Songsakdi.java
+++ LabGit/src/controller/Songsakdi.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class Songsakdi {
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "kuykuy");
+		JOptionPane.showMessageDialog(null, "Welcome to Github Lab.");
 	}
 
 }
